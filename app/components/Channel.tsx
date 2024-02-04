458fcde4c5bf19a9a8e900df37355cc576bcfbf0
DenisC <92527725+GDenisC@users.noreply.github.com>
2024-02-04 11:03:45 +0700

    kafif
    too long line

--- app/components/Channel.tsx
+++ app/components/Channel.tsx
@@ -3,7 +3,7 @@ type Props = {
 }
 
 export default ({ name }: Props) => {
-    return (
-        <button className="text-gray-200 bg-opacity-0 hover:bg-opacity-40 bg-zinc-600 mr-4 ml-4 rounded-lg p-2 font-semibold hover:shadow-lg hover:text-indigo-200 transition ease-in-out duration-300 text-nowrap overflow-hidden text-ellipsis text-left"># {name}</button>
+    return ( /* костыль mr-4 ml-4 можно сменить на flex items-center justify-center но тогда будет строка длинее, transition зачем тут */
+        <button className="text-gray-200 hover:text-indigo-200 hover:bg-zinc-600/40 mr-4 ml-4 rounded-lg p-2 font-semibold hover:shadow-lg transition ease-in-out duration-300 text-nowrap overflow-hidden text-ellipsis"># {name}</button>
     )
 }
