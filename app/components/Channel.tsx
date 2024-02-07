e13cf980dbc648fdf07d0a35eaddb2ad44f68544
vustur <koldasa26@gmail.com>
2024-02-07 18:19:42 +0300

    icons + profile menu

--- app/components/Channel.tsx
+++ app/components/Channel.tsx
@@ -3,7 +3,7 @@ type Props = {
 }
 
 export default ({ name }: Props) => {
-    return ( /* костыль mr-4 ml-4 можно сменить на flex items-center justify-center но тогда будет строка длинее, transition зачем тут */
+    return ( /* транзишион шоб было вау круто */
         <button className="text-gray-200 hover:text-indigo-200 hover:bg-zinc-600/40 mr-4 ml-4 rounded-lg p-2 font-semibold hover:shadow-lg transition ease-in-out duration-300 text-nowrap overflow-hidden text-ellipsis"># {name}</button>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import Channel from "./components/Channel";
 import Server from "./components/Server";
 import Profile from "./components/Profile";
 import React, { useState, useEffect, useRef } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const [showProfile, setShowProfile] = useState(false)
   const [mouseY, setMouseY] = useState(0)
   const [profileId, setProfileId] = useState(0)
   const profile = useRef(null)
+  const [isAdvButttonsEnabled, setIsAdvButttonsEnabled] = useState(false)
 
   const handleMouseMove = (event) => {
     if (showProfile) {
@@ -42,8 +44,8 @@ export default function Home() {
       <div className="w-8/12 lg:w-10/12 bg-zinc-600 shadow-2xl flex flex-col rounded-t-lg">
         <h1 className="text-2xl text-white text-center font-bold bg-zinc-600 w-full h-[6%] shadow-lg rounded-md">ChatName</h1>
         <div className="h-[87%] mr-4 ml-4 overflow-scroll">
-          <Message content="Сложно сказать, почему непосредственные участники технического прогресса объявлены нарушающими общечеловеческие нормы этики и морали!" author="Someone" date={new Date()} id="0" onProfileOpen={() => handleProfileOpen(0)}/>
-          <Message content="Разнообразный и богатый опыт говорит нам, что сплочённость команды профессионалов однозначно фиксирует необходимость глубокомысленных рассуждений." author="Fish" date={new Date()} id="1" onProfileOpen={() => handleProfileOpen(1)} />
+          <Message content="Сложно сказать, почему непосредственные участники технического прогресса объявлены нарушающими общечеловеческие нормы этики и морали!" author="Someone" date={new Date(1707037200000)} id="0" onProfileOpen={() => handleProfileOpen(0)}/>
+          <Message content="Разнообразный и богатый опыт говорит нам, что сплочённость команды профессионалов однозначно фиксирует необходимость глубокомысленных рассуждений." author="Fish" date={new Date(1707037200000)} id="1" onProfileOpen={() => handleProfileOpen(1)} />
         </div>
         <div className="h-[6%] mb-[1%] flex bg-zinc-600">
           <input
@@ -52,11 +54,53 @@ export default function Home() {
             placeholder="Введите сообщение..."
           />
           <button
-            className="h-full w-10 ml-2 mr-2 bg-zinc-500 rounded-lg text-white"
+            className="h-full w-10 ml-2 mr-2 bg-zinc-500 rounded-lg text-white p-1"
             type="button"
           >
-            S
+            <Image src="/icons/send.svg" width={35} height={35} alt="send message" ></Image>
           </button>
+        </div>
+      </div>
+      <div className="absolute w-[calc(41.6666%)] md:w-[calc(33.3333%+3rem)] lg:w-[calc(16.7777%+3.75rem)] bottom-0 left-0 h-14">
+        <div className="w-full h-full bg-gradient-to-r from-zinc-600 to-zinc-700 shadow-lg flex flex-row">
+        { !isAdvButttonsEnabled 
+        ? ( <div className="w-2/5 h-full mt-1">
+            <h1 className="text-xl text-white font-semibold ml-2 -mb-1">Usename</h1>
+            <h1 className="text-sm text-zinc-200 ml-2">@username</h1>
+          </div>
+        )
+        : null
+        }
+          <div className={`w-${isAdvButttonsEnabled ? 'full' : '3/5'} w-full h-full mt-2 flex flex-row-reverse mr-2`}>
+            <button className="w-10 h-10 ml-2 bg-zinc-600 rounded-lg text-white p-1">
+              <Image src="/icons/settings.svg" width={30} height={30} alt="settings"></Image>
+            </button>
+            <button className="w-10 h-10 ml-2 bg-zinc-600 rounded-lg text-white p-1"
+            onClick={() => setIsAdvButttonsEnabled(!isAdvButttonsEnabled)}
+            >
+              {isAdvButttonsEnabled 
+              ? <Image src="/icons/arrow-right.svg" width={30} height={30} alt="arrowRight"></Image> 
+              : <Image src="/icons/arrow-left.svg"  width={30} height={30} alt="arrowLeft"></Image>
+              }
+            </button>
+          { isAdvButttonsEnabled 
+          ? ( /* эво глупо но JSX expression allows only one element!! */
+            <div className="h-full w-max flex flex-row">
+              <button className="w-10 h-10 ml-2 bg-zinc-600 rounded-lg text-white p-1">
+                <Image src="/icons/door.svg" width={30} height={30} alt="plus" ></Image>
+              </button>
+              <button className="w-10 h-10 ml-2 bg-zinc-600 rounded-lg text-white p-1">
+                <Image src="/icons/hand.svg" width={30} height={30} alt="plus"></Image>
+              </button>
+              <button className="w-10 h-10 ml-2 bg-zinc-600 rounded-lg text-white p-1">
+                <Image src="/icons/plus.svg" width={30} height={30} alt="plus"></Image>
+              </button>
+              <p className="text-zinc-500 ml-2 mt-1 text-2xl">|</p>
+            </div>
+          )
+          : null
+          }
+          </div>
         </div>
       </div>
     </div>
